4a3a6d57f1eaf4193d2eddd81b013b5bb3a45d19
Destiny-wei <weihaoming0418@gmail.com>
2023-05-12 23:44:12 +0800

    version1-Wallet的安全显示,通过PrivateKey来保证

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const toHex = require("ethereum-cryptography/utils");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,9 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  // private key: 82d868994e97b07426a2e0d0044333589f4f1284a5b635ffff5cebbbd88c96f4
+  "047ad27c3b9b049cec7e63b0e21faebba349e2a57bbbf385f27abb9649fd1c28847064aa7ec74c6e44b31d08073f4c5c3c42aea3d47b963b8d97dc17f6727ce3be": 100,
+  // private key: 838d1000ad68868020df8de77746cf3f07d95e7169b6f94babdee8a399f5a0ed
+  "0429f0c85f0db71f8d9c02d2880aa46910be833c40f7b87852c07699d1944cf5628a0eef1d399fb5595fda4ad635917ce68aa9cd9e4c0ca2679c8a000ca1b1d317": 50,
+  // private key: cf64a79d04832033b7e90d8535a30dcc7938a2af36101a7f3db741fc4b82c82e
+  "04051039efbcbffa835e88b870fe9d42e551c219fe29c0a4503c9839c78c16157f2e313e1464ebb10ca8bdf0b2bd278bfbbe4bec8afa8ac1a56806ab5cb7fa36ed": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,7 +24,10 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature from the client-side application
+  // recover the public address from signature
   const { sender, recipient, amount } = req.body;
+
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
